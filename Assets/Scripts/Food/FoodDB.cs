140735698b6ac14a9fbd419d3ab426148bf520fd
Ash Villaschi <4luxxx@gmail.com>
2024-08-17 15:22:37 +0100

    Add classes representing food data

---
+++ Assets/Scripts/Food/FoodDB.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UrchinGame.Food {
+    public sealed class FoodDB : ScriptableObject {
+        public ContactFilter2D ContactFilter;
+        public List<FoodData> FoodData = new();
+    }
+}
