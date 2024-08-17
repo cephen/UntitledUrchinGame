4935159568a6f557671c0b8269253e9f8cc5688c
Ash Villaschi <4luxxx@gmail.com>
2024-08-17 16:22:46 +0100

    Add food item monobehaviour

---
+++ Assets/Scripts/Food/FoodItem.cs
@@ -0,0 +1,57 @@
+﻿using Unity.Logging;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace UrchinGame.Food {
+    [RequireComponent(typeof(Collider2D), typeof(SpriteRenderer))]
+    public sealed class FoodItem : MonoBehaviour {
+        private ContactFilter2D _contactFilter;
+        private FoodData _data;
+        private SpriteRenderer _renderer;
+        private float _startTime;
+        private float _startX;
+
+        private FallState _state = FallState.Falling;
+
+        public void Init(FoodData data, ContactFilter2D contactFilter) {
+            _contactFilter = contactFilter;
+            _data = data;
+            _renderer.sprite = _data.Sprite;
+        }
+
+        private enum FallState { Falling, Resting }
+
+#region Unity Lifecycle
+
+        private void Start() {
+            _startX = transform.position.x;
+            _startTime = Time.time;
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+
+        private void FixedUpdate() {
+            if (_state is FallState.Resting) return;
+
+            float phase = math.sin(_data.SwaySpeed * (Time.time - _startTime));
+
+            float2 currPos = (Vector2)transform.position;
+
+            float hPos = _startX + _data.SwayAmount * phase;
+            float vMove = _data.FallSpeed * Time.deltaTime;
+            float vPos = currPos.y - vMove;
+
+            transform.position = new Vector3(hPos, vPos);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other) {
+            Log.Debug("Food {0} touched collider {1}", name, other.gameObject.name);
+
+            bool isTerrain = _contactFilter.IsFilteringLayerMask(other.gameObject);
+
+            if (isTerrain) _state = FallState.Resting;
+        }
+
+#endregion
+    }
+}
