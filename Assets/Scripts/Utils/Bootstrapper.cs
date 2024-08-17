c7c8daf9575759a6dfdae58142b3025325edeec6
Ash Villaschi <4luxxx@gmail.com>
2024-08-17 16:21:11 +0100

    Bootstrap addressables

---
+++ Assets/Scripts/Utils/Bootstrapper.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace UrchinGame.Utils {
+    public static class Bootstrapper {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        private static void InitAddressables() => Addressables.InitializeAsync();
+    }
+}
