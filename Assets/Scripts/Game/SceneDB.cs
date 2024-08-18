60e82552d86162c58798bdfd265b3c40a2fcb73e
Ash Villaschi <fizix@sidefx.dev>
2024-08-18 15:21:53 +0100

    Add SceneDB and set references

---
+++ Assets/Scripts/Game/SceneDB.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace UrchinGame.Game {
+    // [CreateAssetMenu(fileName = "SceneDB", menuName = "Urchin/SceneDB")]
+    internal sealed class SceneDB : ScriptableObject {
+        // Gameplay Scenes
+        [field: SerializeField] public AssetReference MainMenu { get; private set; }
+        [field: SerializeField] public AssetReference Nursery { get; private set; }
+        [field: SerializeField] public AssetReference Racetrack { get; private set; }
+        // Manager Scenes
+        [field: SerializeField] public AssetReference PersistentManagers { get; private set; }
+        [field: SerializeField] public AssetReference GameplayManagers { get; private set; }
+    }
+}
