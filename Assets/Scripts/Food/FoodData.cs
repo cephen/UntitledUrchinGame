f0a8802722cf1aec8b6fb647dfe76a9bccf07977
Alex M <alexmdev04@gmail.com>
2024-08-18 17:07:42 +0100

    Add Shop Images to Food DB entries

--- Assets/Scripts/Food/FoodData.cs
+++ Assets/Scripts/Food/FoodData.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace UrchinGame.Food {
     [Serializable]
     public sealed class FoodData {
         public string Name;
         public Sprite Sprite;
+        public AssetReferenceT<Sprite> UIShopSprite;
         public int Cost = 5;
         public float FallSpeed = 1f;
         public float SwayAmount = 1f;
