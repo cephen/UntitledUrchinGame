12bbcd1aa86c1564b8f8eec77b510756e000c19b
Alex M <alexmdev04@gmail.com>
2024-08-19 16:49:58 +0100

    UI Shop Button now has static background, item icon is retrieved instead
    Moved ui textures to Textures/ui

--- Assets/Scripts/ui/uiShopButton.cs
+++ Assets/Scripts/ui/uiShopButton.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using SideFX.Events;
+using TMPro;
 using Unity.Logging;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -10,9 +12,16 @@ using UrchinGame.Food;
 
 namespace UrchinGame.UI
 {
+    public readonly struct OnShopPurchase : IEvent {
+        public readonly FoodData Food;
+        public OnShopPurchase(FoodData food) => Food = food;
+    }
     public class uiShopButton : MonoBehaviour {
         private Button button;
         private FoodData foodData;
+        [SerializeField] private Image foodIcon;
+        [SerializeField] private TextMeshProUGUI foodName;
+
         private void Awake() {
             button = GetComponent<Button>();
             button.onClick.AddListener(Click);
@@ -21,18 +30,20 @@ namespace UrchinGame.UI
         public void Init(FoodData _foodData) {
             foodData = _foodData;
             foodData.UIShopSprite.LoadAssetAsync().Completed += LoadImageComplete;
-        }
-
-        public void Click() {
-            uiMessage.instance.New("Bought " + foodData.Name, "uiShopButton");
+            foodName.text = foodData.Name;
         }
 
         public void LoadImageComplete(AsyncOperationHandle<Sprite> asyncOperationHandle) {
             if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded) {
-                button.image.sprite = asyncOperationHandle.Result;
+                foodIcon.sprite = asyncOperationHandle.Result;
                 return;
             }
             Log.Error("Failed to load UIShopSprite of " + foodData.Name);
+        }
+
+        public void Click() {
+            uiMessage.instance.New("Bought " + foodData.Name, "uiShopButton");
+            EventBus<OnShopPurchase>.Raise(new OnShopPurchase(foodData));
         }
     }
 }
