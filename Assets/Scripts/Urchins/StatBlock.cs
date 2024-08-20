863eb495f4a185ef00aa8201aa7fb074cdb2caa1
Ash Villaschi <fizix@sidefx.dev>
2024-08-20 13:09:24 +0100

    Add default constructor for StatBlock

--- Assets/Scripts/Urchins/StatBlock.cs
+++ Assets/Scripts/Urchins/StatBlock.cs
@@ -7,5 +7,12 @@ namespace UrchinGame.Urchins {
         public float MaxStamina;
         public float MaxSpeed;
         public float Size;
+
+        public static StatBlock Default() => new StatBlock {
+            Weight = 1f,
+            MaxStamina = 1f,
+            MaxSpeed = 1f,
+            Size = 1f,
+        };
     }
 }
