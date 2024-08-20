4d2769a111e3bcf49ef6560512eb0a5e184d84af
Alex M <alexmdev04@gmail.com>
2024-08-20 17:40:44 +0100

    audio go brr
    debug commands added

--- Assets/Scripts/Game/AudioManager.cs
+++ Assets/Scripts/Game/AudioManager.cs
@@ -5,10 +5,12 @@ using System.Linq;
 using Unity.Logging;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.InputSystem;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UrchinGame.Game;
+using UrchinGame.UI;
 
-namespace UrchinGame {
+namespace UrchinGame.Game {
 
     [System.Serializable]
     public class ListWrapper
@@ -19,29 +21,44 @@ namespace UrchinGame {
 
     public class AudioManager : MonoBehaviour {
         public static AudioManager instance { get; private set; }
+        private AudioSource audioSource;
         [SerializeField] private AssetLabelReference addressableAudioFolder;
-        private HashSet<AudioClip> loadedAudio = new HashSet<AudioClip>();
         private Dictionary<string, List<AudioClip>> sounds = new();
-        public List<ListWrapper> foo= new List<ListWrapper>();
+        public List<AudioSource> loopsPlaying = new();
+        public List<AudioSource> musicIntrosPlaying = new();
+        public bool debugAudioLogs;
+        #if UNITY_EDITOR
+        public Sounds testSound;
+        public List<string> keys = new();
+        #endif
         public enum Sounds {
             none,
             sound_urchinMunch,
             sound_urchinBodySlap,
-            sound_urchinSpikeOut,
-            sound_urchinSpikeIn,
+            sound_urchinSpikesOut,
+            sound_urchinSpikesIn,
             sound_waterPlop,
             sound_uiSelect,
             loop_treadmill,
-            music_urchinSafeGround,
+            music_urchinSafegrounds,
             music_mirrorball
         }
 
         private void Awake() {
             instance = this;
+            audioSource = GetComponent<AudioSource>();
         }
 
         void Start() {
             Addressables.LoadAssetsAsync<AudioClip>(addressableAudioFolder, LoadAudioClipComplete).Completed += LoadAudioComplete;
+        }
+
+        void Update() {
+            #if UNITY_EDITOR
+            if (Keyboard.current[Key.F].wasPressedThisFrame) { PlaySound(testSound); }
+            if (Keyboard.current[Key.G].wasPressedThisFrame) { StopLoopsBySound(testSound); }
+            if (Keyboard.current[Key.H].wasPressedThisFrame) { StopAllLoops(); }
+            #endif
         }
 
         void LoadAudioClipComplete(AudioClip sound) {
@@ -53,42 +70,153 @@ namespace UrchinGame {
                 return;
             }
 
-            string soundBaseName = sound.name[..sound.name.LastIndexOf('_')];
-            if (isVariant)
-            {
+            if (isVariant) {
+                string soundBaseName = sound.name[..sound.name.LastIndexOf('_')];
                 string soundVariant = sound.name[(sound.name.LastIndexOf('_') + 1)..];
-                if (sounds.TryGetValue(soundBaseName, out List<AudioClip> soundEntry)) { Log.Debug(soundBaseName + " new variant " + soundVariant); soundEntry.Add(sound); }
-                else { Log.Debug("new sound base " + soundBaseName); sounds.Add(soundBaseName, new List<AudioClip>() { sound }); }
+                if (sounds.TryGetValue(soundBaseName, out List<AudioClip> soundEntry)) {
+                    Log.Debug(soundBaseName + " new variant: " + soundVariant);
+                    soundEntry.Add(sound);
+                }
+                else {
+                    Log.Debug("new sound base: " + soundBaseName);
+                    sounds.Add(soundBaseName, new List<AudioClip>() { sound });
+                }
             }
             else {
-                sounds.Add(soundBaseName, new List<AudioClip>{ sound });
+                Log.Debug("non-variant sound added: " + sound.name);
+                sounds.Add(sound.name, new List<AudioClip>{ sound });
             }
         }
 
-        void Update() { }
-
         void LoadAudioComplete(AsyncOperationHandle<IList<AudioClip>> asyncOperationHandle) {
             if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded) {
+                #if UNITY_EDITOR
+                foreach (var entry in sounds) { keys.Add(entry.Key + ", " +  entry.Value.Count); }
+                #endif
                 Log.Debug("Audio Directory Load Succeeded");
                 return;
             }
             Log.Error("Audio Directory Load Failed: " + asyncOperationHandle.OperationException);
         }
 
-        public void PlaySound(Sounds sound, bool randomise = true) {
+        public void PlaySound(Sounds sound, bool randomise = true, int variant = 0) {
+            if (sound == Sounds.none) {
+                Log.Warning("[AudioManager] Tried to play 'none' sound");
+                return;
+            }
+            string soundName = Enum.GetName(typeof(Sounds), sound);
+            // assumes enum contains an underscore
+            string soundType = soundName[..soundName.IndexOf('_')];
 
+            switch (soundType) {
+                case "sound": {
+                    if (debugAudioLogs) { Log.Debug("[AudioManager] Playing Sound " + soundName); }
+                    PlaySound(soundName, randomise, variant);
+                    break;
+                }
+                case "loop": {
+                    if (debugAudioLogs) { Log.Debug("[AudioManager] Playing Loop " + soundName); }
+                    PlayLoop(soundName);
+                    break;
+                }
+                case "music": {
+                    if (debugAudioLogs) { Log.Debug("[AudioManager] Playing Music " + soundName); }
+                    PlayMusic(soundName);
+                    break;
+                }
+                default: { // unknown
+                    Log.Warning($"[AudioManager] Unrecognised audio type, playing '{soundName}' as sound");
+                    break;
+                }
+            }
+           // PlaySound(Enum.GetName(typeof(Sounds), sound));
         }
 
-        public void PlayLoop(Sounds sounds) {
-        }
-
-        public void PlayMusic(Sounds music) {
-
-        }
-
-        private void PlaySound(string soundBaseName, bool randomise = true, bool loop = false) {
+        public void PlayLoop(string soundBaseName) {
             if (sounds.TryGetValue(soundBaseName, out List<AudioClip> soundVariants)) {
-                soundVariants[GameManager.instance.random.Next(soundVariants.Count)];
+                AudioSource newLoop = gameObject.AddComponent<AudioSource>();
+                newLoop.loop = true;
+                newLoop.clip = soundVariants[0];
+                newLoop.Play();
+                loopsPlaying.Add(newLoop);
+                return;
+            }
+            Log.Error("[AudioManager] Could not find loop " + soundBaseName);
+
+        }
+
+        public void PlayMusic(string musicName) {
+            if (sounds.TryGetValue(musicName, out List<AudioClip> soundVariants)) {
+                AudioClip[] introClips = soundVariants.Where(static clip => clip.name[clip.name.LastIndexOf('_')..] == "_intro").ToArray();
+                if (introClips.Length < 1) {
+                    PlayLoop(musicName);
+                    return;
+                }
+                AudioSource newIntro = gameObject.AddComponent<AudioSource>();
+                newIntro.clip = introClips[0];
+                newIntro.Play();
+                StartCoroutine(MusicIntroEnd(newIntro));
+                musicIntrosPlaying.Add(newIntro);
+                return;
+            }
+            Log.Error("[AudioManager] Could not find music " + musicName);
+        }
+
+        private void PlaySound(string soundBaseName,  bool randomise = true, int variant = 0, bool loop = false) {
+            if (sounds.TryGetValue(soundBaseName, out List<AudioClip> soundVariants)) {
+                if (randomise) { variant = GameManager.instance.random.Next(soundVariants.Count); }
+                Mathf.Clamp(variant, 0, soundVariants.Count - 1);
+                audioSource.PlayOneShot(soundVariants[variant]);
+                return;
+            }
+            Log.Error("[AudioManager] Could not play sound " + soundBaseName);
+        }
+
+        IEnumerator MusicIntroEnd(AudioSource musicIntroSource) {
+            yield return new WaitForSeconds(musicIntroSource.clip.length);
+            musicIntroSource.Stop();
+
+            if (!sounds.TryGetValue(musicIntroSource.clip.name[..musicIntroSource.clip.name.LastIndexOf('_')],
+                    out List<AudioClip> audioClips)) { yield break; }
+
+            if (audioClips.Count < 1) { yield break; }
+
+            AudioClip[] loopClips = audioClips.Where(static clip => clip.name[clip.name.LastIndexOf('_')..] == "_loop").ToArray();
+            if (loopClips.Length == 0) {
+                Log.Error("[AudioManager] No loop found for " + musicIntroSource.clip.name);
+                yield break;
+            }
+            musicIntroSource.clip = loopClips[0];
+            musicIntroSource.loop = true;
+            musicIntrosPlaying.Remove(musicIntroSource);
+            loopsPlaying.Add(musicIntroSource);
+            musicIntroSource.Play();
+            if (debugAudioLogs) { Log.Debug("[AudioManager] Music Changed to loop: " + loopClips[0].name); }
+        }
+
+        public void StopAllLoops() {
+            foreach (AudioSource loop in loopsPlaying) {
+                loop.Stop();
+                Destroy(loop);
+            }
+            loopsPlaying.Clear();
+        }
+
+        public void StopLoopsBySound(Sounds sound) {
+            for (int i = loopsPlaying.Count - 1; i >= 0; i--) {
+                AudioSource loop = loopsPlaying[i];
+                string loopBaseName = loop.clip.name;
+                if (loop.clip.name.CountOf('_') > 1) {
+                    loopBaseName = loop.clip.name[loop.clip.name.LastIndexOf('_')..];
+                }
+                if (sound.ToString() == loopBaseName) {
+                    loop.Stop();
+                    loopsPlaying.RemoveAt(i);
+                    Destroy(loop);
+                    if (debugAudioLogs) { Log.Debug("[AudioManager] Stopped Loop " + loopBaseName); }
+                    continue;
+                }
+                Log.Warning("[AudioManager] Tried to stop loop '" + loopBaseName + "' but no loop found");
             }
         }
     }
