6d80fb3917ba02a0138529e17928c396a0227e34
Alex M <alexmdev04@gmail.com>
2024-08-24 05:06:06 +0100

    Added simple sprite animation to UI

--- Assets/Scripts/ui/uiMainMenu.cs
+++ Assets/Scripts/ui/uiMainMenu.cs
@@ -1,13 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Logging;
 using UnityEngine;
+using UnityEngine.UI;
+using UrchinGame.Game;
 
 namespace UrchinGame.UI {
     public class uiMainMenu : MonoBehaviour {
-        // Start is called before the first frame update
-        void Start() { }
+        public List<uiAnimatedButton> uiAnimatedButtons = new List<uiAnimatedButton>();
 
-        // Update is called once per frame
-        void Update() { }
+        [Serializable] public struct uiAnimatedButton {
+            public uiAnimatedButton(Button _button, List<Sprite> _sprites, float _timeToSwitchSprite = 0.05f) {
+                button = _button;
+                sprites = _sprites;
+                timeToSwitchSprite = _timeToSwitchSprite;
+                spriteTimer = _timeToSwitchSprite;
+                currentSpriteIndex = 0;
+            }
+
+            public Button button;
+            public List<Sprite> sprites;
+            public float timeToSwitchSprite;
+            public float spriteTimer;
+            public int currentSpriteIndex;
+        }
+        void Update() {
+            for (int i = 0; i < uiAnimatedButtons.Count; i++) {
+                uiAnimatedButton uiAnimatedButton = uiAnimatedButtons[i];
+                uiAnimatedButton.spriteTimer += Time.deltaTime;
+                if (uiAnimatedButton.spriteTimer >= uiAnimatedButton.timeToSwitchSprite) {
+                    uiAnimatedButton.spriteTimer = 0.0f;
+                    uiAnimatedButton.currentSpriteIndex = (uiAnimatedButton.currentSpriteIndex + 1 >= uiAnimatedButton.sprites.Count) ? 0 : (uiAnimatedButton.currentSpriteIndex + 1);
+                    uiAnimatedButton.button.image.sprite = uiAnimatedButton.sprites[uiAnimatedButton.currentSpriteIndex];
+                }
+                uiAnimatedButtons[i] = uiAnimatedButton;
+            }
+        }
     }
 }
