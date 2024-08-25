5f9ae7945cca5d719226940d7a743813aab6af51
Alex M <alexmdev04@gmail.com>
2024-08-25 01:39:41 +0100

    Changed Animated Sprite to a MonoBehaviour that must be used on an Image

--- Assets/Scripts/ui/uiAnimatedSprite.cs
+++ Assets/Scripts/ui/uiAnimatedSprite.cs
@@ -2,24 +2,31 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-[Serializable] public class uiAnimatedImage {
-    public Image image;
-    public List<Sprite> sprites;
+
+[RequireComponent(typeof(Image))]
+[Serializable]
+public class uiAnimatedImage : MonoBehaviour {
+    private Image image;
+    public Sprite[] sprites;
     public float timeToSwitchSprite;
     private float spriteTimer;
     private int currentSpriteIndex;
 
-    public uiAnimatedImage(Image _image, List<Sprite> _sprites, float _timeToSwitchSprite) {
+    public uiAnimatedImage(Image _image, Sprite[] _sprites, float _timeToSwitchSprite) {
         image = _image;
         sprites = _sprites;
         timeToSwitchSprite = _timeToSwitchSprite;
     }
 
-    public void UpdateImage() {
+    public void Awake() {
+        image = GetComponent<Image>();
+    }
+
+    public void Update() {
         spriteTimer += Time.deltaTime;
         if (spriteTimer < timeToSwitchSprite) { return; }
         spriteTimer = 0.0f;
-        currentSpriteIndex = (currentSpriteIndex + 1 >= sprites.Count) ? 0 : (currentSpriteIndex + 1);
+        currentSpriteIndex = (currentSpriteIndex + 1 >= sprites.Length) ? 0 : (currentSpriteIndex + 1);
         image.sprite = sprites[currentSpriteIndex];
     }
 }
--- Assets/Scripts/ui/uiMainMenu.cs
+++ Assets/Scripts/ui/uiMainMenu.cs
@@ -3,9 +3,5 @@ using UnityEngine;
 
 namespace UrchinGame.UI {
     public class uiMainMenu : MonoBehaviour {
-        public List<uiAnimatedImage> animatedImages = new();
-        private void Update() {
-            foreach (uiAnimatedImage animatedImage in animatedImages) { animatedImage.UpdateImage(); }
-        }
     }
 }
